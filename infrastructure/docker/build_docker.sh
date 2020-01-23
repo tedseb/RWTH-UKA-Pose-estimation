f651dca8145fba9c68efb7ffbf0e4e83f0bc1608
Christian Lanius <christian.lanius@rwth-aachen.de>
2020-01-23 21:13:50 +0100

    add initial ros2 docker

--- infrastructure/docker/build_docker.sh
+++ infrastructure/docker/build_docker.sh
@@ -1,8 +1,43 @@
-# Build the base image with the general dependencies
-docker build -t trainerai-base -f infrastructure/docker/base infrastructure/docker
+set -xe
 
-# Build the dev image, which adds dev tools to the image
-docker build -t trainerai-dev -f infrastructure/docker/dev .
+#while [[ $# -gt 0 ]]
+#do
+#key="$1"
+
+PUSH="false"
+BUILD="true"
+
+#case $key in
+#    -p|--push)
+#    EXTENSION=true
+#    shift # past argument
+#    ;;
+#    -s|--skip-build)
+#    SEARCHPATH=false
+#    shift # past argument
+#    ;;
+#    *)    # unknown option
+#    POSITIONAL+=("$1") # save it in an array for later
+#    shift # past argument
+#    ;;
+#esac
+#done
+#set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+if [ "$BUILD" = "true" ] ; then
+    echo 'Building images'
+    # Build the base image with the general dependencies
+    docker build -t registry.git.rwth-aachen.de/trainerai/core/trainerai-base -f infrastructure/docker/base infrastructure/docker
+    # Build the dev image, which adds dev tools to the image
+    docker build -t registry.git.rwth-aachen.de/trainerai/core/trainerai-dev -f infrastructure/docker/dev .
+fi
+
+#if [ "$PUSH" = "true" ] ; then
+#    echo 'Pushing built images to registry'
+#    docker push registry.git.rwth-aachen.de/trainerai/core/trainerai-base
+#    docker push registry.git.rwth-aachen.de/trainerai/core/trainerai-dev
+#fi
 
 
 # Build the release image, which builds the software and packs all the files in it
--- infrastructure/docker/launch_docker.sh
+++ infrastructure/docker/launch_docker.sh
@@ -6,4 +6,4 @@ docker run  -t --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd):/trainerai \
     --device=/dev/video0:/dev/video0 \
-    trainerai-dev & > /dev/null
+    registry.git.rwth-aachen.de/trainerai/core/trainerai-dev & > /dev/null

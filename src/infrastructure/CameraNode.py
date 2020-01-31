f8b92aa786080467e0a35e80d4b86deaeef0b2ef
Christian Lanius <christian.lanius@rwth-aachen.de>
2020-01-31 22:08:53 +0100

    add device id switch to cam

--- src/infrastructure/CameraNode.py
+++ src/infrastructure/CameraNode.py
@@ -3,16 +3,17 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import Image
-
+import sys
+import argparse
 
 class CameraNode(Node):
     
-    def __init__(self):
+    def __init__(self, dev_id=0):
         super().__init__('camera')
         self.pub = self.create_publisher(Image, 'image0', 10)
         timer_period = 0.01
         self.tmr = self.create_timer(timer_period, self.publish_image)
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(dev_id)
         
     def publish_image(self):
         
@@ -28,7 +29,7 @@ class CameraNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = CameraNode()
+    node = CameraNode(0)
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -38,4 +39,7 @@ def main(args=None):
     rclpy.shutdown()
     
 if __name__ == '__main__':
+    #parser = argparse.ArgumentParser(description='Publishes camera images to ros2 topics.')
+    #parser.add_argument('device', dest='device', action='store_const', default=0, help='specifies the device, which is read. Default: 0')
+    #args = parser.parse_args()
     main()

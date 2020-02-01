9dd8549ca2b57fc4b0d78f56cb74e6f54fa5d028
Christian Lanius <christian.lanius@rwth-aachen.de>
2020-02-02 00:42:09 +0100

    reworked to ros1 again

--- src/infrastructure/CameraNode.py
+++ src/infrastructure/CameraNode.py
@@ -1,53 +1,30 @@
+import rospy
 import cv2
-import rclpy
-from rclpy.node import Node
-import numpy as np
 from sensor_msgs.msg import Image
-import sys
-import argparse
-import time
+import numpy as np
 
-
-class CameraNode(Node):
-    
-    def __init__(self, devid=0):
-        super().__init__('camera')
-        self.pub = self.create_publisher(Image, 'image0', 10)
-        timer_period = 0.01
-        #self.tmr = self.create_timer(timer_period, self.publish_image)
-        self.cap = cv2.VideoCapture(0)
+def camera():
+    pub = rospy.Publisher('image', Image, queue_size=2)
+    rospy.init_node('camera', anonymous=True)
+    cap = cv2.VideoCapture(0)
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        if not ret:
+            print('cant read camera', ret)
+            continue
         
-        while True:
-            ret, frame = self.cap.read()
-            if not ret:
-                continue
-            
-            msg = Image()
-            msg.header.frame_id = 'map'
-            t = time.time()
-            msg.header.stamp.sec = int(t)
-            msg.header.stamp.nanosec = 0
-            msg.encoding = "bgr8"
-            msg.data = np.array(frame, dtype=np.uint8).tostring()
-            msg.height, msg.width = frame.shape[:-1]
-            msg.step = frame.shape[-1]*frame.shape[0]
-            self.pub.publish(msg)
-            
+        msg = Image()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'dev0'
+        msg.encoding = "bgr8"
+        msg.data = np.array(frame, dtype=np.uint8).tostring()
+        msg.height, msg.width = frame.shape[:-1]
+        msg.step = frame.shape[-1]*frame.shape[0]
+        pub.publish(msg)
 
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = CameraNode()
-    #try:
-    #    rclpy.spin(node)
-    #except KeyboardInterrupt:
-    #    pass
-
-    node.destroy_node()
-    rclpy.shutdown()
-    
 if __name__ == '__main__':
-    #parser = argparse.ArgumentParser(description='Publishes camera images to ros2 topics.')
-    #parser.add_argument('device', dest='device', action='store_const', default=0, help='specifies the device, which is read. Default: 0')
-    #args = parser.parse_args()
-    main()
+    try:
+        camera()
+    except rospy.ROSInterruptException:
+        pass
+

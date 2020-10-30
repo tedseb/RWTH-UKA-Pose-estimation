b31c1d79d635ca3091071c9f3fce593cec3bd946
shawan.mohammed <shawan.mohammed@rwth-aachen.de>
2020-10-30 12:18:19 +0100

    NODEJS and tfPoseNet

---
+++ src/rosnodejsNodes/POSENode.js
@@ -0,0 +1,200 @@
+const rosnodejs = require('rosnodejs');
+const std_msgs = rosnodejs.require('std_msgs');
+const pose_estimation = rosnodejs.require("pose_estimation");
+
+const tf = require('@tensorflow/tfjs-node');
+const posenet = require('@tensorflow-models/posenet');
+const {createCanvas, Image} = require('canvas')
+const cv = require('opencv4nodejs');
+const Jimp = require('jimp');
+
+const imageScaleFactor = 0.5;
+const outputStride = 16;
+const flipHorizontal = false;
+
+const loadModel = async() => {
+    console.log('start');
+
+    global.net = await posenet.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: 513,
+      multiplier: 1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    });
+}
+
+tryModelIN = async(msg) => {
+  //Check if net-variable has been initialized or defined. Take care is aysnc!
+  if(typeof net !== 'undefined'){
+    //convert from buffer to CV-mat)
+    const matFromArray = new cv.Mat(new Buffer.from(msg.data), msg.height, msg.width, cv.CV_8UC3);
+    
+    //convert from BGR to RGB
+    let mat = matFromArray.cvtColor(cv.COLOR_BGR2RGB);
+        
+    // convert to 3d tensor
+    let buffer = new Uint8Array(mat.getData().buffer);
+    let tFrame = tf.tensor3d(buffer, [640, 480, 3]);
+
+    // net is your poseNet instance
+    let pose = await net.estimateSinglePose(tFrame);
+    for(const keypoint of pose.keypoints) {
+      console.log(`${keypoint.part}: (${keypoint.position.x},${keypoint.position.y})`);
+    }
+
+    const persons_msg = new pose_estimation.msg.Persons;
+    persons_msg.header = msg.header
+    persons_msg.persons = new Array();
+    person_msg = new pose_estimation.msg.Person;
+  
+    for(idx = 0; idx < 25; idx++) {
+      person_msg.bodyParts[idx] = new pose_estimation.msg.Bodypart;
+    }
+   
+    person_msg.bodyParts[0].score = pose.keypoints[0].score;
+    person_msg.bodyParts[0].pixel.x = pose.keypoints[0].position.x/1000;
+    person_msg.bodyParts[0].pixel.y = pose.keypoints[0].position.y/1000;
+    person_msg.bodyParts[0].point.x = 2*(pose.keypoints[0].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[0].point.y = 0.0;
+    person_msg.bodyParts[0].point.z = -2*(pose.keypoints[0].position.y/1000-1.5);
+
+    person_msg.bodyParts[16].score = pose.keypoints[1].score;
+    person_msg.bodyParts[16].pixel.x = pose.keypoints[1].position.x/1000;
+    person_msg.bodyParts[16].pixel.y = pose.keypoints[1].position.y/1000;
+    person_msg.bodyParts[16].point.x = 2*(pose.keypoints[1].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[16].point.y = 0.0;
+    person_msg.bodyParts[16].point.z = -2*(pose.keypoints[1].position.y/1000-1.5);
+
+    person_msg.bodyParts[15].score = pose.keypoints[2].score;
+    person_msg.bodyParts[15].pixel.x = pose.keypoints[2].position.x/1000;
+    person_msg.bodyParts[15].pixel.y = pose.keypoints[2].position.y/1000;
+    person_msg.bodyParts[15].point.x = 2*(pose.keypoints[2].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[15].point.y = 0.0;
+    person_msg.bodyParts[15].point.z = -2*(pose.keypoints[2].position.y/1000-1.5);
+
+    person_msg.bodyParts[18].score = pose.keypoints[3].score;
+    person_msg.bodyParts[18].pixel.x = pose.keypoints[3].position.x/1000;
+    person_msg.bodyParts[18].pixel.y = pose.keypoints[3].position.y/1000;
+    person_msg.bodyParts[18].point.x = 2*(pose.keypoints[3].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[18].point.y = 0.0;
+    person_msg.bodyParts[18].point.z = -2*(pose.keypoints[3].position.y/1000-1.5);
+
+    person_msg.bodyParts[17].score = pose.keypoints[4].score;
+    person_msg.bodyParts[17].pixel.x = pose.keypoints[4].position.x/1000;
+    person_msg.bodyParts[17].pixel.y = pose.keypoints[4].position.y/1000;
+    person_msg.bodyParts[17].point.x = 2*(pose.keypoints[4].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[17].point.y = 0.0;
+    person_msg.bodyParts[17].point.z = -2*(pose.keypoints[7].position.y/1000-1.5);
+
+    person_msg.bodyParts[5].score = pose.keypoints[5].score;
+    person_msg.bodyParts[5].pixel.x = pose.keypoints[5].position.x/1000;
+    person_msg.bodyParts[5].pixel.y = pose.keypoints[5].position.y/1000;
+    person_msg.bodyParts[5].point.x = 2*(pose.keypoints[5].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[5].point.y = 0.0;
+    person_msg.bodyParts[5].point.z = -2*(pose.keypoints[5].position.y/1000-1.5);
+
+    person_msg.bodyParts[2].score = pose.keypoints[6].score;
+    person_msg.bodyParts[2].pixel.x = pose.keypoints[6].position.x/1000;
+    person_msg.bodyParts[2].pixel.y = pose.keypoints[6].position.y/1000;
+    person_msg.bodyParts[2].point.x = 2*(pose.keypoints[6].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[2].point.y = 0.0;
+    person_msg.bodyParts[2].point.z = -2*(pose.keypoints[6].position.y/1000-1.5);
+
+    person_msg.bodyParts[6].score = pose.keypoints[7].score;
+    person_msg.bodyParts[6].pixel.x = pose.keypoints[7].position.x/1000;
+    person_msg.bodyParts[6].pixel.y = pose.keypoints[7].position.y/1000;
+    person_msg.bodyParts[6].point.x = 2*(pose.keypoints[7].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[6].point.y = 0.0;
+    person_msg.bodyParts[6].point.z = -2*(pose.keypoints[7].position.y/1000-1.5);
+
+    person_msg.bodyParts[3].score = pose.keypoints[8].score;
+    person_msg.bodyParts[3].pixel.x = pose.keypoints[8].position.x/1000;
+    person_msg.bodyParts[3].pixel.y = pose.keypoints[8].position.y/1000;
+    person_msg.bodyParts[3].point.x = 2*(pose.keypoints[8].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[3].point.y = 0.0;
+    person_msg.bodyParts[3].point.z = -2*(pose.keypoints[8].position.y/1000-1.5);
+
+    person_msg.bodyParts[7].score = pose.keypoints[9].score;
+    person_msg.bodyParts[7].pixel.x = pose.keypoints[9].position.x/1000;
+    person_msg.bodyParts[7].pixel.y = pose.keypoints[9].position.y/1000;
+    person_msg.bodyParts[7].point.x = 2*(pose.keypoints[9].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[7].point.y = 0.0;
+    person_msg.bodyParts[7].point.z = -2*(pose.keypoints[9].position.y/1000-1.5);
+
+    person_msg.bodyParts[4].score = pose.keypoints[10].score;
+    person_msg.bodyParts[4].pixel.x = pose.keypoints[10].position.x/1000;
+    person_msg.bodyParts[4].pixel.y = pose.keypoints[10].position.y/1000;
+    person_msg.bodyParts[4].point.x = 2*(pose.keypoints[10].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[4].point.y = 0.0;
+    person_msg.bodyParts[4].point.z = -2*(pose.keypoints[10].position.y/1000-1.5);
+
+    person_msg.bodyParts[12].score = pose.keypoints[11].score;
+    person_msg.bodyParts[12].pixel.x = pose.keypoints[11].position.x/1000;
+    person_msg.bodyParts[12].pixel.y = pose.keypoints[11].position.y/1000;
+    person_msg.bodyParts[12].point.x = 2*(pose.keypoints[11].position.x/100-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[12].point.y = 0.0;
+    person_msg.bodyParts[12].point.z = -2*(pose.keypoints[11].position.y/1000-1.5);
+
+    person_msg.bodyParts[9].score = pose.keypoints[12].score;
+    person_msg.bodyParts[9].pixel.x = pose.keypoints[12].position.x/1000;
+    person_msg.bodyParts[9].pixel.y = pose.keypoints[12].position.y/1000;
+    person_msg.bodyParts[9].point.x = 2*(pose.keypoints[12].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[9].point.y = 0.0;
+    person_msg.bodyParts[9].point.z = -2*(pose.keypoints[12].position.y/1000-1.5);
+
+    person_msg.bodyParts[13].score = pose.keypoints[13].score;
+    person_msg.bodyParts[13].pixel.x = pose.keypoints[13].position.x/1000;
+    person_msg.bodyParts[13].pixel.y = pose.keypoints[13].position.y/1000;
+    person_msg.bodyParts[13].point.x = 2*(pose.keypoints[13].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[13].point.y = 0.0;
+    person_msg.bodyParts[13].point.z = -2*(pose.keypoints[13].position.y/1000-1.5);
+
+    person_msg.bodyParts[10].score = pose.keypoints[14].score;
+    person_msg.bodyParts[10].pixel.x = pose.keypoints[14].position.x/1000;
+    person_msg.bodyParts[10].pixel.y = pose.keypoints[14].position.y/1000;
+    person_msg.bodyParts[10].point.x = 2*(pose.keypoints[14].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[10].point.y = 0.0;
+    person_msg.bodyParts[10].point.z = -2*(pose.keypoints[14].position.y/1000-1.5);
+
+    person_msg.bodyParts[14].score = pose.keypoints[15].score;
+    person_msg.bodyParts[14].pixel.x = pose.keypoints[15].position.x/1000;
+    person_msg.bodyParts[14].pixel.y = pose.keypoints[15].position.y/1000;
+    person_msg.bodyParts[14].point.x = 2*(pose.keypoints[15].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[14].point.y = 0.0;
+    person_msg.bodyParts[14].point.z = -2*(pose.keypoints[15].position.y/1000-1.5);
+
+    person_msg.bodyParts[11].score = pose.keypoints[16].score;
+    person_msg.bodyParts[11].pixel.x = pose.keypoints[16].position.x/1000;
+    person_msg.bodyParts[11].pixel.y = pose.keypoints[16].position.y/1000;
+    person_msg.bodyParts[11].point.x = 2*(pose.keypoints[16].position.x/1000-0.5)*msg.width/msg.height;
+    person_msg.bodyParts[11].point.y = 0.0;
+    person_msg.bodyParts[11].point.z = -2*(pose.keypoints[16].position.y/1000-1.5);
+
+  
+    persons_msg.persons.push(person_msg)
+    pub.publish(persons_msg)
+ 
+   
+
+
+
+  }
+}
+
+rosnodejs.initNode('/DasTorZurUnterwelt')
+.then(() => {loadModel()
+});
+const nh = rosnodejs.nh;
+const sub = nh.subscribe('/image', 'sensor_msgs/Image', tryModelIN);
+
+const pub = nh.advertise('/personsJS', 'pose_estimation/Persons');
+/*
+   var len = pose.keypoints.length;
+    for(idx = 0; idx < len; idx++) {
+      person_msg.bodyParts[idx].score = pose.keypoints[idx].score
+      person_msg.bodyParts[idx].pixel.x = pose.keypoints[idx].position.x/100
+      person_msg.bodyParts[idx].pixel.y = pose.keypoints[idx].position.y/100
+      person_msg.bodyParts[idx].point.x = 2*(pose.keypoints[idx].position.x/100-0.5)*msg.width/msg.height
+      person_msg.bodyParts[idx].point.y = 0.0
+      person_msg.bodyParts[idx].point.z = -2*(pose.keypoints[idx].position.y/100-1.5)
+    }*/
---
+++ src/rosnodejsNodes/RESTNode.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const rosnodejs = require('rosnodejs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+rosnodejs.initNode('/DasTorZurUnterwelt')
+.then(() => {
+  // do stuff
+});
+
+const nh = rosnodejs.nh;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  //res.end('Hello World');
+  //console.log('Got msg on chatter bevor');
+  const sub = nh.subscribe('/image', 'sensor_msgs/Image', (msg) => {
+    //console.log('Got msg on chatter: %j', msg);
+    res.end(JSON.stringify(msg));
+
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

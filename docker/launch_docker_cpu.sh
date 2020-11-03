8ea4a5fe22d81401ce11f228acedf9c488e63ff8
Shawan.mohammed <shawan.mohammed@rwth-aachen.de>
2020-11-03 11:49:52 +0100

    CPU launch file

---
+++ docker/launch_docker_cpu.sh
@@ -0,0 +1,10 @@
+docker stop trainerAI > /dev/null
+
+docker run  -t --rm \
+    --name trainerAI \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $(pwd):/trainerai \
+    --device=/dev/video0 \
+    registry.git.rwth-aachen.de/trainerai/core/trainerai-dev-update &2>1 > /dev/null &
+

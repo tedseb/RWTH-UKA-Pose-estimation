c71e0282c370e99bb088ccbe748bda52b72bd7f6
Sebastian/Ted <sebastian.preuss@rwth-aachen.de>
2020-11-13 19:32:43 +0100

    Adjusted launch file. Mount node_modules

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,12 +1,13 @@
-docker stop trainerAI > /dev/null
+docker stop trainerAI &> /dev/null
 
-docker run  -t --rm \
+docker run  -t -d --rm \
     --gpus=all \
     --name trainerAI \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY -e NVIDIA_DRIVER_CAPABILITIES=all \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd):/trainerai \
+    -v /trainerai/node_modules \
     --device=/dev/video0 \
-    registry.git.rwth-aachen.de/trainerai/core/trainerai-dev-update &2>1 > /dev/null &
+    registry.git.rwth-aachen.de/trainerai/core/trainerai-dev-update &> /dev/null &
 

1232a2ccc37421f13318ead7b247fbed397a5e5f
shawan.mohammed <shawan.mohammed@rwth-aachen.de>
2020-12-20 22:27:53 +0100

    check if /parameters is there

--- trainerai.sh
+++ trainerai.sh
@@ -4,6 +4,12 @@ sed -i 's/`//g' ./devel/include/pose_estimation/Persons.h
 sed -i 's/`//g' ./devel/include/pose_estimation/Person.h
 sed -i 's/`//g' ./devel/include/pose_estimation/Bodypart.h
 sed -i 's/`//g' ./devel/include/pose_estimation/Pixel.h
-python3 docker/scripts/SecureGoogleDrive.py
-bash script.sh
-rm script.sh
+
+if [[ ! -d './src/fastpose/parameters/' ]]
+then
+    echo "'./src/fastpose/parameters/' does not exist on your filesystem."
+    python3 docker/scripts/SecureGoogleDrive.py
+    bash script.sh
+    rm script.sh
+fi
+

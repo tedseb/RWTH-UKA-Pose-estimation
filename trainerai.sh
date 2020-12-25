9859f150b77dd3c11f09858408cc4a2fbfe3fcdd
Ted <basti.preuss95@gmail.com>
2020-12-25 18:34:24 +0000

    new sed command in trainerai.sh

--- trainerai.sh
+++ trainerai.sh
@@ -1,9 +1,6 @@
 catkin_make
 source devel/setup.bash
-sed -i 's/`//g' ./devel/include/pose_estimation/Persons.h
-sed -i 's/`//g' ./devel/include/pose_estimation/Person.h
-sed -i 's/`//g' ./devel/include/pose_estimation/Bodypart.h
-sed -i 's/`//g' ./devel/include/pose_estimation/Pixel.h
+find ./devel -type f -name "*.js" -exec sed -i 's/`resolution`//g' {} \;
 
 if [[ ! -d './src/fastpose/parameters/' ]]
 then

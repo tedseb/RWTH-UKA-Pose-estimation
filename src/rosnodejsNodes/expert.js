2188149e7c51eaf8142dfe636ec37302a9d8393b
Tamer Comcuoglu <comcuoglu@outlook.de>
2021-01-23 19:50:40 +0100

    expert ownpose

--- src/rosnodejsNodes/expert.js
+++ src/rosnodejsNodes/expert.js
@@ -9,22 +9,22 @@ const ditw = ['nose', 'leftShoulder', 'rightShoulder',
     'leftHip', 'rightHip', 'leftKnee', 'rightKnee', 'leftAnkle',
     'rightAnkle'];
 
-const ownpose_used = [0, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 28, 37, 39, 41, 42, 43];
+const ownpose_used = [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 28, 37, 39, 41, 42, 43];
 
-const ownpose_labels = [OP_Nose, OP_Neck, OP_R_Shoulder, OP_R_Elblow, OP_R_Wrist, OP_L_Shoulder, OP_L_Elbow, OP_L_Wrist, OP_Middle_Hip, OP_R_Hip, OP_R_Knee, OP_R_Ankle, OP_L_Hip, OP_L_Knee, OP_L_Ankle, OP_R_Eye, OP_L_Eye, OP_R_Ear, OP_L_Ear, OP_L_Big_Toe, OP_L_Small_Toe, OP_L_Heel, OP_R_Big_Toe, OP_R_Small_Toe, OP_R_Heel, R_Ankle, R_Knee, R_Hip, L_Hip9, L_Knee, L_Ankle, R_Wrist, R_Elbow, R_Shoulder, L_Shoulder, L_Elbow, L_Wrist, Neck_LSP, Top_of_Head_LSP, Pelvis_MPII, Thorax_MPII, Spine_HM, Jaw_HM, Head_HM, Nose, L_Eye, R_Eye, L_Ear, R_Ear]
+const ownpose_labels = [OP_Nose, OP_Neck, OP_R_Shoulder, OP_R_Elbow, OP_R_Wrist, OP_L_Shoulder, OP_L_Elbow, OP_L_Wrist, OP_Middle_Hip, OP_R_Hip, OP_R_Knee, OP_R_Ankle, OP_L_Hip, OP_L_Knee, OP_L_Ankle, OP_R_Eye, OP_L_Eye, OP_R_Ear, OP_L_Ear, OP_L_Big_Toe, OP_L_Small_Toe, OP_L_Heel, OP_R_Big_Toe, OP_R_Small_Toe, OP_R_Heel, R_Ankle, R_Knee, R_Hip, L_Hip9, L_Knee, L_Ankle, R_Wrist, R_Elbow, R_Shoulder, L_Shoulder, L_Elbow, L_Wrist, Neck_LSP, Top_of_Head_LSP, Pelvis_MPII, Thorax_MPII, Spine_HM, Jaw_HM, Head_HM, Nose, L_Eye, R_Eye, L_Ear, R_Ear]
 
 const ownpose = [
     [2, 3], [3, 4], [5, 6], [6, 7], [27, 9], [9, 12], [27, 28], [27, 10], [10, 11], [12, 13], [9, 10], [28, 12], [28, 13], [13, 14], [14, 21], [21, 20], [21, 19], [20, 19], [11, 24], [24, 22], [22, 23], [23, 24], [5, 28], [2, 27], [5, 2], [42, 17], [42, 18], [42, 0], [0, 15], [0, 16], [15, 16], [17, 43], [18, 43], [1, 37], [37, 43], [41, 37], [41, 39]
 ]
 
 const angle_points = {
-    leftLeg: ['leftHip', 'leftKnee', 'leftAngle'],
-    leftArm: ['leftShoulder', 'leftElbow', 'leftWrist'],
-    leftShin: ['leftKnee', 'leftAnkle'],
-    rightLeg: ['rightHip', 'rightKnee', 'rightAngle'],
-    rightArm: ['rightShoulder', 'rightElbow', 'rightWrist'],
-    rightShin: ['rightKnee', 'rightAnkle'],
-    upperBody: ['rightShoulder', 'leftShoulder', 'leftHip', 'rightHip']
+    leftLeg: ['OP_L_Hip', 'OP_L_Knee', 'OP_L_Ankle'],
+    leftArm: ['OP_L_Shoulder', 'OP_L_Elbow', 'OP_L_Wrist'],
+    leftShin: ['OP_L_Knee', 'OP_L_Ankle'],
+    rightLeg: ['OP_R_Hip', 'OP_R_Knee', 'OP_R_Ankle'],
+    rightArm: ['OP_R_Shoulder', 'OP_R_Elbow', 'OP_R_Wrist'],
+    rightShin: ['OP_R_Knee', 'OP_R_Ankle'],
+    upperBody: ['OP_R_Shoulder', 'OP_R_Hip', 'OP_R_Knee', 'OP_L_Shoulder', 'OP_L_Hip', 'OP_L_Knee']
 }
 
 
@@ -99,16 +99,34 @@ const sub = nh.subscribe('/personsJS', 'pose_estimation/Persons', (msg) => {
     let pose = {};
     this.coordinates = msg;
     let bodyParts = msg.persons[0]['bodyParts'];
-    let labels = ditw;
+    /* let labels = ditw;
     labels.forEach((label, index) => {
         let point = {};
         point.x = bodyParts[index].point.x;
         point.y = bodyParts[index].point.z;
         point.z = bodyParts[index].point.y;
         pose[label] = point;
+    }); */
+
+    ownpose_used.forEach(index => {
+        let point = {};
+        point.x = bodyParts[index].point.x;
+        point.y = bodyParts[index].point.z;
+        point.z = bodyParts[index].point.y;
+        pose[ownpose_labels[index]] = point;
+    });
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(pose));
+        }
     });
     lastPose = pose;
-    angles.leftLeg = threepointangle(pose.leftHip, pose.leftKnee, pose.leftAnkle);
+    let entries = Object.entries(angle_points);
+    for(const [angle, points] in entries) {
+        angles[angle] = calculateAngle(points);
+    } 
+    console.log(angles);
+    /* angles.leftLeg = threepointangle(pose.leftHip, pose.leftKnee, pose.leftAnkle);
     angles.rightLeg = threepointangle(pose.rightHip, pose.rightKnee, pose.rightAnkle);
     angles.leftArm = threepointangle(pose.leftShoulder, pose.leftElbow, pose.leftWrist);
     angles.rightArm = threepointangle(pose.rightShoulder, pose.rightElbow, pose.rightWrist);
@@ -116,7 +134,7 @@ const sub = nh.subscribe('/personsJS', 'pose_estimation/Persons', (msg) => {
     const bottomLeft = { x: pose.leftAnkle.x, y: pose.leftAnkle.y - 1, z: pose.leftAnkle.z };
     const bottomRight = { x: pose.rightAnkle.x, y: pose.rightAnkle.y - 1, z: pose.rightAnkle.z };
     angles.leftShin = threepointangle(pose.leftKnee, pose.leftAnkle, bottomLeft);
-    angles.rightShin = threepointangle(pose.rightKnee, pose.rightAnkle, bottomRight);
+    angles.rightShin = threepointangle(pose.rightKnee, pose.rightAnkle, bottomRight); */
     count();
     correct();
 });
@@ -133,6 +151,27 @@ correct = () => {
         if (messages.length > 0) { error_pub.publish({ data: messages }) };
     }
 
+};
+
+calculateAngle = (arr) => {
+    switch(arr.length) {
+        case 2:
+            let newPoint = {
+                x: arr[1].x,
+                y: arr[1].y - 1,
+                z: arr[1].z
+            };
+            return threepointangle(lastPose[arr[0]], lastPose[arr[1]], newPoint);
+            break;
+        case 3:
+            return threepointangle(lastPose[arr[0]], lastPose[arr[1]], lastPose[arr[2]]);
+            break;
+        case 6:
+            return (threepointangle(lastPose[arr[0]], lastPose[arr[1]], lastPose[arr[2]]) + threepointangle(lastPose[arr[3]], lastPose[arr[4]], lastPose[arr[5]])) / 2;
+            break;
+        default:
+            return 0;
+    }
 };
 
 count = () => {

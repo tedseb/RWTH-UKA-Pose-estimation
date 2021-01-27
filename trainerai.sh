cd33d36211c2fd38acea7399e0ce2f535e5256a6
Artur Niederfahrenhorst <artur.niederfahrenhorst@rwth-aachen.de>
2021-01-27 16:14:12 +0100

    fixed trainerai.sh

--- trainerai.sh
+++ trainerai.sh
@@ -1,12 +1,20 @@
+# This script downloads and build all files neccessary for the TrainerAI project
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+# catkin_make builds the project with catkin in the folders src, build and evel
 catkin_make
-source devel/setup.bash
+# catkin generates a setup file that sets a couple of environment variables 
+sh $SCRIPTPATH/devel/setup.sh
+# TODO: @Shawan What does this line do? Is there a link you copied this from?
 find ./devel -type f -name "*.js" -exec sed -i 's/`resolution`//g' {} \;
 
 if [[ ! -d './src/fastpose/parameters/' ]]
 then
     echo "'./src/fastpose/parameters/' does not exist on your filesystem."
     python3 docker/scripts/SecureGoogleDrive.py
-    bash script.sh
+    sh script.sh
     rm script.sh
 fi
 
@@ -16,7 +24,7 @@ then
     echo "./src/spin/extra_data/body_module/' does not exist on your filesystem."
     echo ">>  Download extra data for body module"
     python3 docker/scripts/spin/SecureGoogleDrive.py
-    bash script.sh
+    sh script.sh
     rm script.sh
 fi
 

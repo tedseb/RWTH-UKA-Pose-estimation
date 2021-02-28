28c2b21bfe573a2c9dec5a798b2c58b7994b37c7
Optimus <shawan.mohammed@rwth-aachen.de>
2021-02-28 02:45:20 +0100

    expert system running on docker uri change

--- src/rosnodejsNodes/expert_system.js
+++ src/rosnodejsNodes/expert_system.js
@@ -1,16 +1,54 @@
 var MongoClient = require('mongodb').MongoClient;
 const rosnodejs = require('rosnodejs');
 const StringMsg = rosnodejs.require('std_msgs').msg.String;
-const YAML = require('yaml')
+const YAML = require('yaml');
 
-rosnodejs.initNode('/expert_slim')
+squats = {
+  'name': 'squats',
+  'stages': [
+      {
+          'angles': {
+              'leftLeg': 165,
+              'rightLeg': 165,
+              'upperBody': 165
+          },
+          'rules': {
+              'upperBody': ["min", 140, "Bitte achte darauf, dass dein Oberkoerper beim Stehen gerade ist"]
+          },
+          'name': "Anfangsposition"
+      },
+      {
+          'angles': {
+              'leftLeg': 125,
+              'rightLeg': 125,
+              'upperBody': 125
+          },
+          'rules': {},
+          'name': "Transition"
+      },
+      {
+          'angles': {
+              'leftLeg': 90,
+              'rightLeg': 90,
+              'upperBody': 90
+          },
+          'rules': {
+              'leftHipKneeToe': ["behind", 160, "Bitte achte darauf, in der Hocke dein linkes Knie nicht ueber deine Zehenspitzen zu lassen"],
+              'rightHipKneeToe': ["behind", 160, "Bitte achte darauf, in der Hocke dein rechtes Knie nicht ueber deine Zehenspitzen zu lassen"],
+          },
+          'name': "Hocke"
+      }
+  ]
+}
+
+rosnodejs.initNode('/expert_system')
     .then(() => {});
 
 const nh = rosnodejs.nh;
 const pubex = nh.advertise('/exercises', StringMsg);
 
 // Connect to the db
-const uri = "mongodb://localhost:27017/";
+const uri = "mongodb://mongoadmin:secret@localhost:27888/?authSource=admin";
 
 const client = new MongoClient(uri);
 
@@ -20,19 +58,17 @@ async function run() {
     const db = client.db("trainerai");
     const exercises = db.collection("exercises");
 
-    nh.subscribe('/qr_exercise', StringMsg, (msg) => {
+    const interim = exercises.insertOne(squats);
+
+    nh.subscribe('/qr_exercise', StringMsg, async (msg) => {
         const query = { name: msg };
         const result = await exercises.findOne(query);
         const stringified = YAML.stringy(result);
+        console.log(stringified);
         nh.setParam('exercise',stringified);
         pubex.publish({data: 'exercise'});
     });
 
-    // Query for a movie that has the title 'The Room'
-    const query = { name: "squats" };
-
-    const result = await exercises.findOne(query);
-    console.log(result);
   } finally {
     await client.close();
   }

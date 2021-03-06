d60f6cb64dc9fd6401e8293e8b9c375341deae58
Artur Niederfahrenhorst <artur.niederfahrenhorst@rwth-aachen.de>
2021-03-07 00:26:39 +0100

    comparing system changes for alpha

--- src/comparing_system/src/config.py
+++ src/comparing_system/src/config.py
@@ -12,7 +12,7 @@ ROS_JOINTS_TOPIC = 'fused_skelleton' # Shawan's topic choice
 ROS_EXERCISES_CHANGE_TOPIC = 'exercises' # TODO: Change this from tamer
 ROS_SKELLETON_DEFINITION = 'skelleton_name'
 
-NUMBER_OF_COMPARATOR_THREADS = 4
+NUMBER_OF_COMPARATOR_THREADS = 20
 
 REDIS_MAXIMUM_QUEUE_SIZE = 100
 REDIS_MAXIMUM_PAST_QUEUE_SIZE = 100
@@ -20,13 +20,12 @@ STATION_QUEUE_SIZE_MINIMUM = 0
 
 REDIS_USER_STATE_SENDING_QUEUE_NAME = "user_state_sending_queue"
 REDIS_USER_INFO_SENDING_QUEUE_NAME = "user_correction_sending_queue"
+REDIS_LOAD_BALANCER_LIST_KEY = "spot_queue_load_balancer_list"
 
 QUEUEING_USER_STATE_QUEUE_SIZE_MAX = 100
 QUEUEING_USER_INFO_QUEUE_SIZE_MAX = 100
 
 assert STATION_QUEUE_SIZE_MINIMUM <= REDIS_MAXIMUM_QUEUE_SIZE
-
-
 
 # Incase other people want to use the same redis database, better use a prefix for keys so our data does not collide
 REDIS_GENERAL_PREFIX = "ComparingSystem:"

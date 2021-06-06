3775f5e796eaf118a30a80a00df636a2718ab008
Artur Niederfahrenhorst <artur.niederfahrenhorst@rwth-aachen.de>
2021-06-06 09:53:04 +0200

    Refactored Comparator

--- src/comparing_system/src/Comparator.py
+++ src/comparing_system/src/Comparator.py
@@ -123,6 +123,124 @@ def custom_metric(hankel_matrix, feature_trajectory, max_weight_, min_weight):
     return errors
 
 
+def compare_high_level_features(spot_info_dict: dict, 
+    last_feature_progressions: dict,
+    last_resampled_features: dict,
+    features_states: dict) -> Tuple[bool, dict, Any]:
+    """Compare high level features, such as angles, by extracting them from the joints array.
+    
+    This method turn high level features into a progression dictionary and resamples them.
+    By calculating the progression dictionary it also detects repetitions.
+
+    Args:
+        spot_info_dict: Contains the exercise data that we want to compare our user's features to.
+        last_feature_progressions: The feature progressions dictionary from the previous comparing step
+        last_resampled_features: The resampled values from possibly many previous comparings steps
+        feature_states: The current feature states
+        
+    Returns:
+        increase_reps (bool): Is true if a repetition was detected
+        new_feature_progressions (dict): Resembles the features of interest specification dictionary but has strings inplace for every feature
+                                            that indicate the progress the user made concerning said feature.
+        new_resampled_features (dict): Resembles the features of interest specification dictionary but has lists inplace for every feature
+                                        that contain the resampled values.
+    """
+    exercise_data = spot_info_dict['exercise_data']
+    beginning_states = spot_info_dict['exercise_data']['beginning_state_dict']
+
+    increase_reps = True
+    new_feature_progressions = {}
+    new_resampled_features = {}
+
+    for feature_type, features in features_states.items():
+        new_feature_progressions[feature_type] = {}
+        new_resampled_features[feature_type] = {}
+        for k, v in features.items():
+            # With the current feature value and the last resampled feature value, we can compute new resampled feature values
+            feature_value = features_states[feature_type][k]['feature_value']
+            resolution = exercise_data['reference_feature_data'][feature_type][k]['range_of_motion'] * FEATURE_TRAJECTORY_RESOLUTION_FACTOR
+            scale = exercise_data['reference_feature_data'][feature_type][k]['resampled_reference_trajectory_scale_array']
+            try:
+                last_resampled_feature_value = float(last_resampled_features[feature_type][k])
+                new_resampled_feature_values = compute_resampled_feature_values(feature_value, last_resampled_feature_value, resolution)
+                if new_resampled_feature_values:
+                    new_resampled_features[feature_type][k] = new_resampled_feature_values
+            except KeyError:
+                # If we have no resampled feature values yet, set them to the nearest resampled one
+                new_resampled_features[feature_type][k] = scale[np.argmin(abs(scale - feature_value))]
+            
+            # With the beginning state of a feature and the current feature state, we can computer the new feature progression value
+            try:
+                last_feature_progression = last_feature_progressions[feature_type][k]
+            except KeyError:
+                # If we have no last feature progression value, set this feature progression to the starting value
+                last_feature_progression = PROGRESSION_START
+            beginning_state = beginning_states[feature_type][k]['feature_state']
+            features_state = features_states[feature_type][k]['feature_state']
+            new_feature_progression = compute_new_feature_progression(beginning_state, features_state, last_feature_progression)
+            
+            # If one of the features is not done, the repetition is not done
+            if new_feature_progression != PROGRESSION_DONE:
+                increase_reps = False
+            # We only need to update the feature progression state if the progress has changed
+            if (new_feature_progression != last_feature_progression):
+                new_feature_progressions[feature_type][k] = new_feature_progression
+        # If a data type has no updates, remove it again
+        if new_feature_progressions[feature_type] == {}:
+            del new_feature_progressions[feature_type]
+        if new_resampled_features[feature_type] == {}:
+            del new_resampled_features[feature_type]
+    
+    # TODO: Do something safe here, this might not reset all features
+    if increase_reps:
+        def reset_child_featuers(d):
+            for k, v in d.items():
+                if isinstance(v, collections.MutableMapping):
+                    d[k] = reset_child_featuers(v)
+                else:
+                    d[k] = PROGRESSION_START
+                return d
+        new_feature_progressions = reset_child_featuers(new_feature_progressions)
+
+    return increase_reps, new_feature_progressions, new_resampled_features
+
+def calculate_reference_pose_mapping(feature_trajectories: dict, exercise_data: dict) -> np.ndarray:
+    """Calculate the pose in the reference trajectory that we think our user is most probably in.
+
+    This method measures the similarity between the recent feature_trajectory of a user and the vectors
+    inside a hankel matrix of the reference trajectory. Thereby we can compute how likely it is that the
+    use is at a certain point in the execution of the exercise of the expert.
+
+    Args:
+        feature_trajectories: A dictionary that holds a list of past feature values for every type of feature
+                                in every feature category
+        exercise data: A dictionary containing metadata around the exercise
+
+    Returns:
+        reference_pose: The reference pose that we think the user is in
+    """
+    reference_poses = exercise_data['recording']
+    predicted_indices = []
+
+    for feature_type, features in exercise_data['reference_feature_data'].items():
+        for k, v in features.items():
+            resampled_values_reference_trajectory_indices = v['resampled_values_reference_trajectory_indices']
+            reference_trajectory_hankel_matrices = v['reference_trajectory_hankel_matrices']
+            feature_trajectory = np.asarray(feature_trajectories[feature_type][k], np.float16)
+
+            for idx, reference_trajectory_hankel_matrix in enumerate(reference_trajectory_hankel_matrices):
+                errors = custom_metric(reference_trajectory_hankel_matrix, feature_trajectory, 4, 0)
+                prediction = np.argmin(errors)
+                index = resampled_values_reference_trajectory_indices[idx][prediction]
+
+                predicted_indices.append(index)
+
+    predicted_pose_index = int(np.average(predicted_indices))
+    reference_pose = reference_poses[predicted_pose_index]
+    
+    return reference_pose
+
+
 class Comparator(Thread):
     """Pops data from inbound spot queues and calculates and metrics which are put into outbound message queues.
     
@@ -185,8 +303,13 @@ class Comparator(Thread):
                 # As long as there are skelletons available for this spot, continue
                 past_joints_with_timestamp_list, joints_with_timestamp, future_joints_with_timestamp_list = self.spot_queue_interface.dequeue(self.spot_key)
 
+                # Extract feature states
+                exercise_data = spot_info_dict['exercise_data']
+                used_joint_ndarray = joints_with_timestamp['used_joint_ndarray']
+                features_states = self.feature_extractor.extract_states(used_joint_ndarray, exercise_data['reference_feature_data'], exercise_data['feature_of_interest_specification'])
+
                 # Compare joints with expert system data
-                increase_reps, new_features_progression, new_resampled_features = self.compare_high_level_features(spot_info_dict, last_feature_progressions, last_resampled_features, joints_with_timestamp)
+                increase_reps, new_features_progression, new_resampled_features = compare_high_level_features(spot_info_dict, last_feature_progressions, last_resampled_features, features_states)
 
                 # Enqueue data for feature progressions and resampled feature lists
                 self.past_features_queue_interface.enqueue(spot_feature_progression_key, new_features_progression)
@@ -211,7 +334,7 @@ class Comparator(Thread):
                 # Calculate a new reference pose mapping
                 if new_resampled_features:
                     all_resampled_features = self.past_resampled_features_queue_interface.get_features(spot_resampled_features_key, latest_only=False)
-                    reference_pose = self.calculate_reference_pose_mapping(all_resampled_features, spot_info_dict['exercise_data'])
+                    reference_pose = calculate_reference_pose_mapping(all_resampled_features, spot_info_dict['exercise_data'])
                     reference_body_parts = self.feature_extractor.ndarray_to_body_parts(reference_pose)
                     reference_person_msg = Person()
                     reference_person_msg.stationID = 99
@@ -246,133 +369,3 @@ class Comparator(Thread):
                 if HIGH_VERBOSITY:
                     print_exc() 
                     rp.logerr("Encountered an Error while Comparing: " + str(e))    
-
-    def compare_high_level_features(self, 
-    spot_info_dict: dict, 
-    last_feature_progressions: dict,
-    last_resampled_features: dict,
-    joints_with_timestamp: list) -> Tuple[bool, dict, Any]:
-        """Compare high level features, such as angles, by extracting them from the joints array.
-        
-        This method turn high level features into a progression dictionary and resamples them.
-        By calculating the progression dictionary it also detects repetitions.
-
-        Args:
-            spot_info_dict: Contains the exercise data that we want to compare our user's features to.
-            last_feature_progressions: The feature progressions dictionary from the previous comparing step
-            last_resampled_features: The resampled values from possibly many previous comparings steps
-            joints_with_timestamp: The joints and theirs timestamp for the current comparing step
-            
-        Returns:
-            increase_reps (bool): Is true if a repetition was detected
-            new_feature_progressions (dict): Resembles the features of interest specification dictionary but has strings inplace for every feature
-                                             that indicate the progress the user made concerning said feature.
-            new_resampled_features (dict): Resembles the features of interest specification dictionary but has lists inplace for every feature
-                                           that contain the resampled values.
-        """
-        exercise_data = spot_info_dict['exercise_data']
-        used_joint_ndarray = joints_with_timestamp['used_joint_ndarray']
-        timestamp = joints_with_timestamp['ros_timestamp']
-        beginning_states = spot_info_dict['exercise_data']['beginning_state_dict']
-
-        features_states = self.feature_extractor.extract_states(used_joint_ndarray, exercise_data['reference_feature_data'], exercise_data['feature_of_interest_specification'])
-        
-        increase_reps = True
-        new_feature_progressions = {}
-        new_resampled_features = {}
-
-        for feature_type, features in features_states.items():
-            new_feature_progressions[feature_type] = {}
-            new_resampled_features[feature_type] = {}
-            for k, v in features.items():
-                # With the current feature value and the last resampled feature value, we can compute new resampled feature values
-                feature_value = features_states[feature_type][k]['feature_value']
-                resolution = exercise_data['reference_feature_data'][feature_type][k]['range_of_motion'] * FEATURE_TRAJECTORY_RESOLUTION_FACTOR
-                scale = exercise_data['reference_feature_data'][feature_type][k]['resampled_reference_trajectory_scale_array']
-                try:
-                    last_resampled_feature_value = float(last_resampled_features[feature_type][k])
-                    new_resampled_feature_values = compute_resampled_feature_values(feature_value, last_resampled_feature_value, resolution)
-                    if new_resampled_feature_values:
-                        new_resampled_features[feature_type][k] = new_resampled_feature_values
-                except KeyError:
-                    # If we have no resampled feature values yet, set them to the nearest resampled one
-                    new_resampled_features[feature_type][k] = scale[np.argmin(abs(scale - feature_value))]
-                
-                # With the beginning state of a feature and the current feature state, we can computer the new feature progression value
-                try:
-                    last_feature_progression = last_feature_progressions[feature_type][k]
-                except KeyError:
-                    # If we have no last feature progression value, set this feature progression to the starting value
-                    last_feature_progression = PROGRESSION_START
-                beginning_state = beginning_states[feature_type][k]['feature_state']
-                features_state = features_states[feature_type][k]['feature_state']
-                new_feature_progression = compute_new_feature_progression(beginning_state, features_state, last_feature_progression)
-                
-                # If one of the features is not done, the repetition is not done
-                if new_feature_progression != PROGRESSION_DONE:
-                    increase_reps = False
-                # We only need to update the feature progression state if the progress has changed
-                if (new_feature_progression != last_feature_progression):
-                    new_feature_progressions[feature_type][k] = new_feature_progression
-            # If a data type has no updates, remove it again
-            if new_feature_progressions[feature_type] == {}:
-                del new_feature_progressions[feature_type]
-            if new_resampled_features[feature_type] == {}:
-                del new_resampled_features[feature_type]
-        
-        # TODO: Do something safe here, this might not reset all features
-        if increase_reps:
-            def reset_child_featuers(d):
-                for k, v in d.items():
-                    if isinstance(v, collections.MutableMapping):
-                        d[k] = reset_child_featuers(v)
-                    else:
-                        d[k] = PROGRESSION_START
-                    return d
-            new_feature_progressions = reset_child_featuers(new_feature_progressions)
-
-        return increase_reps, new_feature_progressions, new_resampled_features
-
-    def calculate_reference_pose_mapping(self, feature_trajectories: dict, exercise_data: dict) -> np.ndarray:
-        """Calculate the pose in the reference trajectory that we think our user is most probably in.
-
-        This method measures the similarity between the recent feature_trajectory of a user and the vectors
-        inside a hankel matrix of the reference trajectory. Thereby we can compute how likely it is that the
-        use is at a certain point in the execution of the exercise of the expert.
-
-        Args:
-            feature_trajectories: A dictionary that holds a list of past feature values for every type of feature
-                                  in every feature category
-            exercise data: A dictionary containing metadata around the exercise
-
-        Returns:
-            reference_pose: The reference pose that we think the user is in
-        """
-        reference_poses = exercise_data['recording']
-        predicted_indices = []
-
-        for feature_type, features in exercise_data['reference_feature_data'].items():
-            for k, v in features.items():
-                resampled_values_reference_trajectory_indices = v['resampled_values_reference_trajectory_indices']
-                reference_trajectory_hankel_matrices = v['reference_trajectory_hankel_matrices']
-                feature_trajectory = np.asarray(feature_trajectories[feature_type][k], np.float16)
-
-                # TODO: Do this for all hankel matrices
-                for idx, reference_trajectory_hankel_matrix in enumerate(reference_trajectory_hankel_matrices):
-                    errors = custom_metric(reference_trajectory_hankel_matrix, feature_trajectory, 4, 0)
-                    prediction = np.argmin(errors)
-                    index = resampled_values_reference_trajectory_indices[idx][prediction]
-
-                    predicted_indices.append(index)
-
-
-                    # print("\\\\\\\\\\\\\\\\")
-                    # print("prediction", prediction)
-                    # print("reference idx", resampled_values_reference_trajectory_indices[idx])
-                    # print("errors", errors)
-                    # print("index", index)
-                    # print("len", len(reference_poses))
-
-        reference_pose = reference_poses[predicted_indices[0]]
-        
-        return reference_pose

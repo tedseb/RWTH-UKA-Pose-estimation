503312dbe60b1e3fc5708628656ddefa6387f607
Sebastian Ted Andreas Preuß <sebastian.preuss@rwth-aachen.de>
2021-08-25 19:27:32 +0200

    [NOT WORKING] added weight detection service loop

--- src/AI/weight_detection/weight_detection.py
+++ src/AI/weight_detection/weight_detection.py
@@ -3,9 +3,16 @@ import sys
 import time
 import rospy
 import numpy as np
+import cv2
 from backend.srv import WeightDetection as RosWeightDetection
 from backend.srv import WeightDetectionResponse, WeightDetectionRequest
+from rospy.exceptions import ROSException
 from sensor_msgs.msg import Image
+from src.yolo_model import YoloWeightModel
+from src.utils import crop_weights_into_image
+from src.dumbbell import Dumbbell
+#pylint: disable=unused-wildcard-import
+from src.config import *
 
 VERBOSE_MODE = True
 def LOG_DEBUG(msg, debug = VERBOSE_MODE):
@@ -15,11 +22,21 @@ def LOG_DEBUG(msg, debug = VERBOSE_MODE):
 def LOG_INFO(msg):
     rospy.loginfo(f"[WD] {msg}", logger_name="StationManager")
 
+def LOG_ERROR(msg):
+    rospy.logerr(f"[WD] {msg}", logger_name="StationManager")
+
+def ros_image_to_cv_image(img):
+    shape = img.height, img.width, 3
+    img = np.frombuffer(img.data, dtype=np.uint8)
+    img = img.reshape(shape)
+    return img
+
 class WeightDetection:
     def __init__(self, debug=False, verbose=False):
         self._verbose = verbose
         self._debug = debug
         self._weigth_detection = rospy.Service('sm/weight_detection', RosWeightDetection, self.handle_weight_detection_request)
+        self._model = YoloWeightModel()
         print("init")
 
     def start(self):
@@ -29,14 +46,46 @@ class WeightDetection:
     def handle_weight_detection_request(self, req : WeightDetectionRequest):
         image_channel = "Image"
         time_limit = 2
-        sub = rospy.Subscriber(image_channel, Image, self.process_image)
-        time.sleep(time_limit)
-        print("test")
 
-    def process_image(self, img_msg):
-        shape = img_msg.height, img_msg.width, 3                            #(480, 640, 3) --> (y,x,3)
-        img = np.frombuffer(img_msg.data, dtype=np.uint8)
-        img_original_bgr = img.reshape(shape)
+        #Todo: Mehrere Frames warten
+        try:
+            frame = rospy.wait_for_message(image_channel, Image, timeout=5)
+        except ROSException:
+            LOG_ERROR("No Image on the channel '{image_channel}'")
+
+        frame = ros_image_to_cv_image(frame)
+        frame = cv2.resize(frame, (1280, 720))
+        output = self._model.inference(frame)
+        pred = output.pred
+
+        if self._model.is_valid_prediction(pred):
+            left_box = self._model.get_left_box(pred)
+            right_box = self._model.get_right_box(pred)
+            dumbbell = Dumbbell(left_box, right_box, frame)
+        else:
+            LOG_ERROR("No Valid Prediction")
+
+        dumbbell = Dumbbell(left_box, right_box, frame)
+        sub = rospy.Subscriber(image_channel, Image, self.process_image, callback_args=(dumbbell))
+        time.sleep(time_limit)
+        sub.unregister()
+
+        barcode_left, barcode_right = dumbbell.barcodes()
+
+        weight_left = 0
+        for code in barcode_left:
+            weight_left += COLOR_RANGES[code][1]
+
+        weight_right = 0
+        for code in barcode_right:
+            weight_right += COLOR_RANGES[code][1]
+
+        return WeightDetectionResponse(weight_left + weight_right)
+
+    def process_image(self, img_msg, dumbbell : Dumbbell):
+        img = ros_image_to_cv_image(img_msg)
+        img = cv2.resize(img, (1280, 720))
+        dumbbell.add_masks(img)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

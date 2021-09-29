dc830c55545bc3f869dc7750794e17467340513b
Sebastian Preuß <basti.preuss95@gmail.com>
2021-09-29 15:22:51 +0200

    station selection client update

--- src/infrastructure/station_manager/src/station_selection.py
+++ src/infrastructure/station_manager/src/station_selection.py
@@ -9,7 +9,8 @@ import copy
 from typing import Callable
 from websocket import create_connection
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem, QDialog
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread, QObject, QtGui
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread, QObject
+from PyQt5.QtGui import QIntValidator
 
 if __name__=="__main__":
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -40,6 +41,7 @@ class StationSelection(StationSelectionUi, QObject):
         self.setupUi(self._main_window)
         self._is_connected = False
         self._data_manager : DataManager = data_manager
+        self.activate_exercise_button.setEnabled(False)
         time.sleep(1)
         try:
             self._web_socket = create_connection(MOBILE_SERVER)
@@ -53,8 +55,8 @@ class StationSelection(StationSelectionUi, QObject):
         self._id = id_data["id"]
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-        only_int = QtGui.QIntValidator()
-        self.exercise_edit.setValidator(self.only_int)
+        only_int = QIntValidator()
+        self.exercise_edit.setValidator(only_int)
 
         self.station_active = False
         self.exercise_active = False
@@ -72,10 +74,9 @@ class StationSelection(StationSelectionUi, QObject):
         self.toggle_station()
         index = self.station_combobox.currentIndex()
         station_id = self.station_combobox.itemData(index, Qt.UserRole)
-        is_active = self.active_box.isChecked()
         data = copy.deepcopy(REQUEST_DICT)
         data["id"] = self._id
-        data["request"] = 1 if is_active else 2
+        data["request"] = 1 if self.station_active else 2
         data["payload"] = {"station" : station_id, "exercise" : int(-1)}
         json_str_station = json.dumps(data)
         
@@ -87,44 +88,38 @@ class StationSelection(StationSelectionUi, QObject):
                 print(f"Could not connect to {MOBILE_SERVER}")
 
         if self._is_connected:
-            if is_active:
-                self._web_socket.send(json_str_station)
-                self.output_text.append(json_str_station)
-                print(self._web_socket.recv())
-                self._web_socket.send(json_str_exercise)
-                self.output_text.append(json_str_exercise)
-            else:
-                self._web_socket.send(json_str_exercise)
-                self.output_text.append(json_str_exercise)
-                print(self._web_socket.recv())
-                self._web_socket.send(json_str_station)
-                self.output_text.append(json_str_station)
+            self._web_socket.send(json_str_station)
+            self.output_text.append(json_str_station)
+            print(self._web_socket.recv())
 
     def set_exercise_state(self):
         self.toggle_exercise()
         exercise = self.exercise_edit.text()
         data = copy.deepcopy(REQUEST_DICT)
+        index = self.station_combobox.currentIndex()
+        station_id = self.station_combobox.itemData(index, Qt.UserRole)
         data["id"] = self._id        
         data["request"] = 3 if self.exercise_active else 4
         data["payload"] = {"station" : station_id, "exercise" : int(exercise)}
         data["payload"]["set_id"] = 1
         json_str_exercise = json.dumps(data)
         
-
         if self._is_connected:
-                self._web_socket.send(json_str_station)
-                self.output_text.append(json_str_station)
-                print(self._web_socket.recv())
-                self._web_socket.send(json_str_exercise)
-                self.output_text.append(json_str_exercise)
+            self._web_socket.send(json_str_exercise)
+            self.output_text.append(json_str_exercise)
+            print(self._web_socket.recv())
 
     def toggle_exercise(self): 
         if self.exercise_active:
             self.exercise_edit.setEnabled(True)
-            self.activate_exercise_button.setText("Stop Exercise")
+            self.activate_exercise_button.setText("Start Exercise")
+            self.activate_station_button.setEnabled(True)
+            self.weight_detection_button.setEnabled(True)
         else:
             self.exercise_edit.setEnabled(False)
-            self.activate_exercise_button.setText("Start Exercise")
+            self.activate_exercise_button.setText("Stop Exercise")
+            self.activate_station_button.setEnabled(False)
+            self.weight_detection_button.setEnabled(False)
 
         self.exercise_active = not self.exercise_active
 
@@ -132,9 +127,13 @@ class StationSelection(StationSelectionUi, QObject):
         if self.station_active:
             self.station_combobox.setEnabled(True)
             self.activate_station_button.setText("Activate Station")
+            self.activate_exercise_button.setEnabled(False)
+            self.weight_detection_button.setEnabled(False)
         else:
             self.station_combobox.setEnabled(False)
             self.activate_station_button.setText("Deactivate Station")
+            self.activate_exercise_button.setEnabled(True)
+            self.weight_detection_button.setEnabled(True)
 
         self.station_active = not self.station_active
 

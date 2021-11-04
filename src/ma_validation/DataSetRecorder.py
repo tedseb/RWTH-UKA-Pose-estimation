cdc65a7d3cc5000b45eb75311a3c69f4ceba6772
Ted <sebastian.preuss@rwth-aachen.de>
2021-11-04 18:35:39 +0100

    untested and incomplete

--- src/ma_validation/DataSetRecorder.py
+++ src/ma_validation/DataSetRecorder.py
@@ -13,30 +13,71 @@ import argparse
 import sys
 import pathlib
 import sched, time
+import yaml
  
 from station_manager import StationManager, signal_handler
 
-from backend.msg import Persons
+from msg import MAValidation
 
 
 class DataSetRecorder():
     def __init__(self, 
     station_manager: StationManager,
-    input_folder: str,
-    output_file: str):
+    input_video_file_path: str,
+    timecode_file_path: str,
+    output_file_path: str):
         self.station_manager = station_manager
         # Define a subscriber to retrive tracked bodies
-        rp.Subscriber(ROS_JOINTS_TOPIC, Persons, self.callback)
-        self.spot_queue_interface = spot_queue_interface_class()
+        self.validation_set_publisher = rp.Publisher("ma_validation_sets", MAValidation, queue_size=100)
+        self.video_timing_subscriber = rp.Subscriber("ma_validation_video_timing", float, self.callback)
 
-        self.spot_queue_load_balancer = spot_queue_load_balancer_class()
-        self.feature_extractor = feature_extractor_class()
-
+        with open(timecode_file_path) as stream:
+            try:
+                timecode_data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+                return
         
+        if pathlib.Path(input_video_file_path).name != timecode_data.file_name:
+            rp.logerr("Input video file name does not match file name specified in timecode file")
 
-    def callback(self, message: Any) -> None:
-        pass
-        
+        def get_t_from(list_item):
+            return list_item.t_from
+
+        self.set_list = timecode_data.sets.sort(key=get_t_from)
+        self.done_sets = []
+        self.active_set = None
+
+
+    def callback(self, t: float) -> None:
+        def are_we_past_t_from(_set):
+            if t > _set.t_from:
+                return True
+            else:
+                return False
+
+        if are_we_past_t_from(self.active_set):
+            set_message = MAValidation()
+            set_message.exercise_id = current_set.exercise_id
+            set_message.t_from = current_set.t_from
+            set_message.t_to = current_set.t_to
+            set_message.repetitions = current_set.repetitions
+            set_message.start = False
+            self.active_set = None
+
+
+        while are_we_past_t_from(self.set_list[0]):
+            current_set = self.set_list.pop(0)
+            set_message = MAValidation()
+            set_message.exercise_id = current_set.exercise_id
+            set_message.t_from = current_set.t_from
+            set_message.t_to = current_set.t_to
+            set_message.repetitions = current_set.repetitions
+            set_message.start = True
+            self.validation_set_publisher.publish(set_message)
+
+            self.active_set = current_set
+
 
 if __name__ == '__main__':
     rp.init_node('DataSetRecorder', anonymous=False)
@@ -64,3 +105,7 @@ if __name__ == '__main__':
     DataSetRecorder = DataSetRecorder(station_manager, input_folder=args.input, output_file=parser.out)
 
     rp.spin()
+
+    self.video_timing_publisher = rp.Publisher("ma_validation_video_timing", float, queue_size=100)
+
+    # TODO: Tell station manager to publish 

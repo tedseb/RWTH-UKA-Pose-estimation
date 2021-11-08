dca4f79e2cab86a438598f22e1b64d5b963ceeff
Ted <sebastian.preuss@rwth-aachen.de>
2021-11-08 10:12:45 +0100

    fixed missing source setup.sh line

--- trainerai.sh
+++ trainerai.sh
@@ -7,18 +7,18 @@ SCRIPTPATH="/home/trainerai/trainerai-core"
 source /opt/ros/noetic/setup.bash
 catkin_make
 # catkin generates a setup file that sets a couple of environment variables
-sh $SCRIPTPATH/devel/setup.sh
+source $SCRIPTPATH/devel/setup.sh
 # TODO: @Shawan What does this line do? Is there a link you copied this from?
 find ./devel -type f -name "*.js" -exec sed -i 's/`resolution`//g' {} \;
 
 echo ">>  Start downloading large files from Google Drive"
-if [[ ! -d './src/AI/spin/extra_data/body_module' ]]
-then
-    echo "./src/AI/spin/extra_data/body_module/' does not exist on your filesystem."
-    echo ">>  Download extra data for body modules"
-    python3 docker/scripts/largeFiles/SecureGoogleDrive.py
-    sh script.sh
-    rm script.sh
-fi
+# if [[ ! -d './src/AI/spin/extra_data/body_module' ]]
+# then
+#     echo "./src/AI/spin/extra_data/body_module/' does not exist on your filesystem."
+#     echo ">>  Download extra data for body modules"
+#     python3 docker/scripts/largeFiles/SecureGoogleDrive.py
+#     sh script.sh
+#     rm script.sh
+# fi
 
 

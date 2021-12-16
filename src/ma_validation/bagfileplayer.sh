2d4b6a436cdd43f82e02c396ab2975923e7c08a0
Ted <basti.preuss@gmail.com>
2021-12-16 16:01:15 +0100

    we can now interrupt an optimization

--- src/ma_validation/bagfileplayer.sh
+++ src/ma_validation/bagfileplayer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/trainerai-core/data
 # This line is supposed to play back our rosbag file with 100x speed, waiting for the message queue interface to report back on the queue length of the motion analysis every second 
-rosbag play dataset.bag # -r 100 --rate-control-topic="ma_validation_rosbag_rate_control" --rate-control-max-delay=1 
+rosbag play __name:=Bagfileplayer dataset.bag # -r 100 --rate-control-topic="ma_validation_rosbag_rate_control" --rate-control-max-delay=1 

dff417bd7613357d049fb03cc1e22e23b944bfe9
Sebastian Preuß <basti.preuss95@gmail.com>
2022-09-16 18:29:22 +0200

    fix: install tensorrt

--- src/lib/tools/install_tensorrt.sh
+++ src/lib/tools/install_tensorrt.sh
@@ -10,11 +10,12 @@ then
   exit -1
 fi
 echo "Install Tensorflow and Create engine"
-# sudo apt-get install python3-libnvinfer-dev
-# pip3 install nvidia-tensorrt -U --index-url https://pypi.ngc.nvidia.com
+sudo apt-get install python3-libnvinfer-dev
+pip3 install nvidia-tensorrt -U --index-url https://pypi.ngc.nvidia.com
 # python3 export.py --weights yolov5m.pt --include engine --batch_size 2
 
 if [[ ! -d "$OBJ_DETECT_PATH/yolov5m.engine" ]]
 then
     python3 "$YOLO_PATH/export.py" --weights "$OBJ_DETECT_PATH/yolov5m.pt" --include engine --batch-size 2 --device 0 --half
 fi
+# python3 /home/trainerai/trainerai-core/src/AI/object_detection/yolov5/export.py --weights /home/trainerai/trainerai-core/src/AI/object_detection/yolov5m.pt --include engine --batch-size 2 --device 1 --half

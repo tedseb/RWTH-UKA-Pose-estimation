27860469ce4545aa5d11274aba4283e4a6827d24
Sebastian/Ted <basti.preuss95@gmail.com>
2022-09-28 21:03:41 +0200

    code update

--- src/lib/tools/load_awinda.py
+++ src/lib/tools/load_awinda.py
@@ -13,14 +13,18 @@ import tensorflow as tf
 import rospy
 
 PATH_MVNX = "data/awinda/Test-007#Hannah2.mvnx"
-PATH_VIDEO = "data/awinda/Test-007_drehen#Hannah2.mp4"
+# PATH_VIDEO = "data/awinda/Test-007_drehen#Hannah2.mp4"
+PATH_VIDEO = "data/double_squats_02.mp4"
 METRABS_PATH = "/home/trainerai/trainerai-core/src/AI/metrabs/models/metrabs_eff2m_y4"
-USE_METRABS = False
+USE_METRABS = True
 COMPARE_ANGLES = [
     ["LeftUpperLeg", "LeftLowerLeg", "LeftFoot"],
     ["RightUpperLeg", "RightLowerLeg", "RightFoot"]
 ]
-
+SMPL_CONNECTIONS = [
+    (1, 4), (1, 0), (2, 5), (2, 0), (3, 6), (3, 0), (4, 7), (5, 8), (6, 9), (7, 10), (8, 11), (9, 12), (12, 13),
+    (12, 14), (12, 15), (13, 16), (14, 17), (16, 18), (17, 19), (18, 20), (19, 21), (20, 22), (21, 23)
+]
 
 def unit_vector(vector):
     return vector / np.linalg.norm(vector)
@@ -30,6 +34,20 @@ def angle_between(v1, v2):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)) * (180 / np.pi)
+
+
+# Todo: Import this from lib
+class TraceTime:
+    def __init__(self, name):
+        self._name = name
+        self._time_stamp = 0
+
+    def __enter__(self):
+        self._time_stamp = time.time()
+
+    def __exit__(self, type, value, traceback):
+        time_elapsed = (time.time() - self._time_stamp) * 1000
+        print(f"Time {self._name}: {time_elapsed}ms")
 
 
 class AwindaDataToRos:
@@ -63,8 +81,14 @@ class AwindaDataToRos:
     def send_metrabs(self, image):
         if self._model is None:
             return
-        pred = self._model.detect_poses(image)
-        print("metrabs")
+
+        with TraceTime("Metrabs"):
+            pred = self._model.detect_poses(
+                image, skeleton='smpl_24', default_fov_degrees=55, detector_threshold=0.5)
+
+        positions = pred['poses3d'].numpy()[0]
+        print(positions)
+        self.send_ros_markers(positions, SMPL_CONNECTIONS, "dev1", ColorRGBA(0.98, 0.30, 0.30, 1.00))
 
     @staticmethod
     def read_awinda_mvnx(file_name: str):
@@ -85,7 +109,8 @@ class AwindaDataToRos:
         for joint in joints:
             connection = []
             for connector in joint:
-                connection.append(connector.text.split("/")[0])
+                connector_joint_name = connector.text.split("/")[0]
+                connection.append(points.index(connector_joint_name))
             connections.append(connection)
         return (points, connections)
 
@@ -113,18 +138,18 @@ class AwindaDataToRos:
             print("yz:", yz)
             print("angle", angle_between(a, b))
 
-    def send_ros_markers(self, positions, connections):
+    def send_ros_markers(self, positions, connections, frame_id="dev0", color=ColorRGBA(0.98, 0.30, 0.30, 1.00)):
         idx = 0
         marker_array = MarkerArray()
 
-        for position in positions.values():
+        for position in positions:
             m = Marker()
             m.header.stamp = rospy.Time.now()
-            m.header.frame_id = "dev0"
+            m.header.frame_id = frame_id
             m.id = idx
             idx += 1
             m.ns = ''
-            m.color = ColorRGBA(0.98, 0.30, 0.30, 1.00)
+            m.color = color
             m.scale = self._skeleton_scale
             m.pose.position.x, m.pose.position.y, m.pose.position.z = position
             m.type = 2
@@ -135,13 +160,13 @@ class AwindaDataToRos:
         for connection in connections:
             m = Marker()
             m.header.stamp = rospy.Time.now()
-            m.header.frame_id = "dev0"
+            m.header.frame_id = frame_id
             m.id = idx
             idx += 1
             m.ns = ''
-            m.color = ColorRGBA(0.98, 0.30, 0.30, 1.00)
+            m.color = color
             m.scale = self._connection_scale
-            m.points = [Point(*positions[(connection[0])]), Point(*positions[connection[1]])]
+            m.points = [Point(*positions[connection[0]]), Point(*positions[connection[1]])]
             m.type = 4
             m.action = 0
             m.lifetime = rospy.Duration(0.2)
@@ -175,9 +200,10 @@ class AwindaDataToRos:
                 point_posittions[point_names[i]] = positions[i * 3:(i + 1) * 3]
 
             self.compute_angles(point_posittions)
-            self.send_ros_markers(point_posittions, connections)
             self.send_video()
-
+            if USE_METRABS:
+                self.send_metrabs(self._cur_frame)
+            self.send_ros_markers(list(point_posittions.values()), connections)
 
             frame_time_stamp_ms = float(frame.attrib["time"])
             time_now_ms = time.time() * 1000
@@ -224,3 +250,63 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+import numpy as np
+import procrustes
+
+def main():
+    x = np.array([[0, 0, 1], [0, 1, 1]])
+    y = np.array([[2, 0, 0], [2, 2, 0]])
+
+    d, Z, tform = procrustes.procrustes(x, y, True, False)
+    T = tform['rotation']
+    b = tform['scale']
+    c = tform['translation']
+    print(T)
+    print(b)
+    print(c)
+    y_new = b * y @ T + c
+    print("###### should be #####")
+    print(x)
+    print("######   is now  #####")
+    print(y_new)
+
+
+def main2():
+    p1 = np.array([1, 1, 1])
+    p2 = np.array([0, 0, 0])
+    p3 = np.array([1, 0, 1])
+    a = p1 - p2
+    b = p3 - p2
+    print(a)
+    print(b)
+    xy = np.arctan2(a[0], a[1]) - np.arctan2(b[0], b[1])
+    xz = np.arctan2(a[0], a[2]) - np.arctan2(b[0], b[2])
+    yz = np.arctan2(a[1], a[2]) - np.arctan2(b[1], b[2])
+    xy = np.absolute(xy) * (180 / np.pi)
+    xz = np.absolute(xz) * (180 / np.pi)
+    yz = np.absolute(yz) * (180 / np.pi)
+
+    print(xy)
+    print(xz)
+    print(yz)
+
+
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)) * (180 / np.pi)
+
+
+def main3():
+    a = np.array([0.31, -1.64, 0.42])
+    b = np.array([-0.39, -0.04, -1.13])
+    print(angle_between(a, b))
+
+
+if __name__ == "__main__":
+    main3()
